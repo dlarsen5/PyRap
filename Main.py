9aeb6b14496b6adf40937e132bd5d28ce6bd1dcc
dlarsen5 <dklarsen@live.com>
2017-11-10 20:00:49 -0500

    Updated package to newer NLP model framework and refractoring
    Signed-off-by: dlarsen5 <dklarsen@live.com>

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from Model import Train_And_Test
+from Retrieve_Lyrics import Get_Lyrics
 
 
 if __name__ == '__main__':
 
     #Download any artists not already in 'Song Lyrics' folder
 
-    #Get_Lyrics(['Justin Bieber'])
+    Get_Lyrics(['Justin Bieber'])
 
     #train and test model using artists' songs in 'Song Lyrics' folder
     #input is number of artists to pull from 'Song Lyrics' folder and input to NLP model
